7baae0cd4fb7f5f45321c0736c76cc773d33127c
Anirban0011 <bhattandbhatt.999@gmail.com>
2026-04-10 14:27:41 +0530

    upd

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import UserRoutes from './routes/users.routes.js'
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN.split(','),
     credentials: true
 }
 ))
