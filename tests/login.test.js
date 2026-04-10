01f4e428e5fd8f34ea45d29060bd7adb71c580de
Anirban0011 <bhattandbhatt.999@gmail.com>
2026-04-10 19:29:42 +0530

    upd login test workflow

--- tests/login.test.js
+++ tests/login.test.js
@@ -8,14 +8,17 @@ const TestLogin = async()=>{
     try{
     const command = `
     curl --silent --location \
-     -w "\\n%{http_code}" \
-    '${process.env.BASE_URL}/users/login' \
+    -w "\\n%{http_code}" \
+    '${process.env.BASE_URL}/api/${process.env.API_VERSION}/users/login'
     -H 'Content-Type: application/json' \
     -d '{
         "username" : "${process.env.TEST_USERNAME}",
         "password": "${process.env.TEST_PASSWORD}"
     }'`
-    const { stdout} = await execAsync(command)
+    console.log(command)
+    const { stdout, stderr} = await execAsync(command)
+    console.log("STDERR >>>", stderr)
+    console.log("STDOUT >>>", stdout)
     const [body, status] = stdout.split('\n')
     const response = JSON.parse(body)
      if (status !== `200`) {
