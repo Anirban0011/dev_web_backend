94ad2d6128847347bd1d3a8a3dd2be24837e548d
Anirban <bhattandbhatt.999@gmail.com>
2026-04-14 22:25:13 +0530

    test : test inbox mail feature 9 (#14)

--- src/services/ReceiveMail.js
+++ src/services/ReceiveMail.js
@@ -4,30 +4,29 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 
 const forwardMail = async(payload)=>{
     const emailid = payload?.email_id
-    const {data : received, error} = await resend.emails.receiving.get(emailid)
+    const {data : received} = await resend.emails.receiving.get(emailid)
 
-    if (error) {
-        console.error("Error fetching email:", error)
-        return
-    }
-
-    let contentHtml = "<p><em>No content</em></p>"
-
-    if (received.html && received.html.trim()) {
-        contentHtml = received.html;
-    } else if (received.text && received.text.trim()) {
-        contentHtml = `<div style="white-space: pre-wrap;">${received.text.replace(/\n/g, '<br>')}</div>`
-    }
+    const contentHtml = received.html || `<div style="white-space: pre-wrap;">${received.text}</div>`
 
     await resend.emails.send({
         from: "Anirban Builds <contact@anirbanbuilds.online>",
         to : MAIL_ID,
-        subject : `${received.subject || payload.subject} || New Message`,
+        subject : `${received.subject || payload.subject || "New Message"}`,
         html: `
         <div style="font-family:sans-serif;line-height:1.6;">
             <hr/>
             <p><strong>From:</strong> ${received.from}</p>
-            <p>${contentHtml}</p>
+           <div class="gmail_quote">
+                <blockquote style="margin:0 0 0 0.8ex; border-left:1px #ccc solid; padding-left:1ex;">
+
+                    <p style="color:#777;">--- Original Message ---</p>
+
+                    <div style="color:#222;">
+                        ${contentHtml}
+                    </div>
+
+                </blockquote>
+            </div>
         </div>
         `
     })
