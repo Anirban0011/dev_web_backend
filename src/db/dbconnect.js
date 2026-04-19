216485eca437b94977e949f7bd83b8eb4eabbbcf
Anirban0011 <bhattandbhatt.999@gmail.com>
2026-04-19 11:14:29 +0530

    test : test db change 1

--- src/db/dbconnect.js
+++ src/db/dbconnect.js
@@ -1,10 +1,12 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 import {DB_NAME} from "../constants.js"
 
 const connectDB = async () => {
     try {
-        const db_instance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const dbname = process.env.APP_MODE === 'local' ? 'dev' : 'prod'
+        const db_instance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}_${dbname}`)
         console.log(`\nMongoDB connected at host : ${db_instance.connection.host}`)
+        console.log(`database mode : ${dbname}`)
 
     } catch (error) {
         console.log("DB Connection Error", error)
