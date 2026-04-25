f0c1a78e8d64699770ec90e9627fe31221dede8a
Anirban <bhattandbhatt.999@gmail.com>
2026-04-25 11:38:36 +0530

    regular feature fix 3 (#43)

--- scripts/gitcommit.ps1
+++ scripts/gitcommit.ps1
@@ -4,7 +4,10 @@ function ship($branch, $message) {
     git commit -m $message
     git push origin $branch
     git checkout master
+    git merge $branch
     git branch -D $branch
 
+    $currentBranch = git branch --show-current
+    Write-Host "$currentBranch  updated ✅" -ForegroundColor Green
     Write-Host " 🚀 Deployment initiated! " -ForegroundColor Cyan
 }
